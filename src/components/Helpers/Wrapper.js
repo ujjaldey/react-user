d878e7b31c2205502f1dcaeb7228631372d55da2
Ujjal Dey <ujjaldey@gmail.com>
2022-04-04 08:27:00 +0800

    #012

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Wrapper = (props) => {
   // children prop holds all the content you are passing between the opening and closing tag of your custom component.
   // so here you are returning only those children elements between the opening and closing tag.
